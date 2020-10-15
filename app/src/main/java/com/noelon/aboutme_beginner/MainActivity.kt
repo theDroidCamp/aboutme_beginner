4de4da158496354e1a7c7f5912c8111f828a45f6
Noel-elon <noelnwaelugo@gmail.com>
2020-10-15 06:31:46 +0100

    about droidcamp

--- app/src/main/java/com/noelon/aboutme_beginner/MainActivity.kt
+++ app/src/main/java/com/noelon/aboutme_beginner/MainActivity.kt
@@ -1,11 +1,25 @@
 package com.noelon.aboutme_beginner
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        droidcamp_button.setOnClickListener {
+            showToast("Droidcamp button clicked")
+            //Todo: Replace "SomeActivity::class with your activity name then remove the comment
+//            val intent = Intent(this, SomeActivity::class.java)
+//            startActivity(intent)
+        }
+    }
+
+    fun showToast(message: String) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
     }
 }
